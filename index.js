42a0dce63c8357a0410a24919e58096bf9460cd3
Uday Kumar <forudaykumar2019@gmail.com>
2025-06-14 11:50:14 +0530

    2nd commit

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log(a);
 
 const b=30, c=40;
 console.log(b+c);
+
+console.log('Create a New File')
+
+console.log("Your new Git repository is empty.");
+let e=20;
+let d=30;
+console.log(e-d);
